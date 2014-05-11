82a08367e82f839c664fe45a6e55e14c94746c42
santi <santi@localhost.localdomain>
2014-05-10 21:46:43 -0300

    inicio proyecto

---
+++ src/main/java/org/trabajo/App.java
@@ -0,0 +1,13 @@
+package org.trabajo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
