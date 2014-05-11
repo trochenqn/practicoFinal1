4559e59885e06d91eb3893305271c57b5c573c0f
santi <santi@localhost.localdomain>
2014-05-10 22:48:43 -0300

    fist commit

--- src/main/java/org/trabajo/App.java
+++ src/main/java/org/trabajo/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "esto es otro saludo!" );
+        
     }
+    
+    
+    
 }
